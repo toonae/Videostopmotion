aac28bdf113e7c097fd8ebefaa439668c30bdf0b
toonae <sup.ta1909@gmail.com>
2018-10-10 18:29:02 +0700

    ใหม่วีดีโอ02

--- app/src/main/java/com/example/computer/videostopmotion/MainActivity.java
+++ app/src/main/java/com/example/computer/videostopmotion/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.computer.videostopmotion;
 
+import android.media.MediaPlayer;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton volumnImageButton;
+    MediaPlayer mediaPlayer;
 
     private TabLayout tabLayout;
     private AppBarLayout appBarLayout;
@@ -19,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
         viewPager= (ViewPager) findViewById(R.id.viewpaper_id);
+
+
+        volumnImageButton = findViewById(R.id.volume);
+        mediaPlayer = MediaPlayer.create(this, R.raw.mgangoom);
+        volumnImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                }else {
+                    mediaPlayer.start();
+
+                }
+
+            }
+        });
+
         ViewPaperAdapter adapter = new ViewPaperAdapter(getSupportFragmentManager());
 
         adapter.AddFragment(new FragmentQuiz(),"บทประพันธ์");
@@ -27,5 +49,8 @@ public class MainActivity extends AppCompatActivity {
         adapter.AddFragment(new FragmentDetail(),"ส่วนผสม");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+    public void clickBack(View view) {
+        finish();
     }
 }
